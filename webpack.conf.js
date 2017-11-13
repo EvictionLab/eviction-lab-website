ceb2195a5f77c222ed8ee0908586b7e1af7af1fb
Lane Olson <lane@onlea.org>
2017-11-12 20:05:31 -0700

    initial commit of sample site

---
+++ webpack.conf.js
@@ -0,0 +1,38 @@
+import webpack from "webpack";
+import path from "path";
+
+export default {
+  module: {
+    loaders: [
+      {
+        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file?name=/[hash].[ext]"
+      },
+      {test: /\.json$/, loader: "json-loader"},
+      {
+        loader: "babel",
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        query: {cacheDirectory: true}
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      "fetch": "imports?this=>global!exports?global.fetch!whatwg-fetch"
+    })
+  ],
+
+  context: path.join(__dirname, "src"),
+  entry: {
+    app: ["./js/app"],
+    cms: ["./js/cms"]
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  externals:  [/^vendor\/.+\.js$/]
+};
