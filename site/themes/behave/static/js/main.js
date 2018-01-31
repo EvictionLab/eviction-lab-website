e53d125488217a700d76ef24ab57edf6c60e908f
Jonny Miller <jonnymiller@selfdesign.org>
2018-01-31 11:15:40 -0800

    hero button smooth scroll to pictorial menu

--- site/themes/behave/static/js/main.js
+++ site/themes/behave/static/js/main.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($){
 	var isLateralNavAnimating = false;
-	
+
 	//open/close lateral navigation
 	$('.cd-nav-trigger').on('click', function(event){
 		event.preventDefault();
-		//stop if nav animation is running 
+		//stop if nav animation is running
 		if( !isLateralNavAnimating ) {
-			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
-			
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true;
+
 			$('body').toggleClass('navigation-is-open');
 			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 				//animation is over
@@ -15,4 +15,46 @@ jQuery(document).ready(function($){
 			});
 		}
 	});
+
+	// start SMOOTH SCROLLING - https://codepen.io/chriscoyier/pen/dpBMVP
+
+	// Select all links with hashes
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+	      &&
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
+
+	// end SMOOTH SCROLLING
+
 });
