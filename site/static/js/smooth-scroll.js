6aeb0692a59d09f86925d89bd9d7d62ee0c41636
James Minton <james-minton@github.com>
2018-02-10 21:49:19 -0500

    Smooth scroll update
    - Added new smooth scroll script in footer that doesn't conflict
    with Bootstrap accordion

---
+++ site/static/js/smooth-scroll.js
@@ -0,0 +1,20 @@
+$(function() {
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('.smoothScroll').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000); // The number here represents the speed of the scroll in milliseconds
+          return false;
+        }
+      }
+    });
+  });
+  
+  // Change the speed to whatever you want
+  // Personally i think 1000 is too much
+  // Try 800 or below, it seems not too much but it will make a difference
