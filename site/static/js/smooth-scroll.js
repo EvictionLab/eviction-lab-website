8df4e425350a414ce08004ee6c61f20f0a2fbf3a
pjsier <pjsier@gmail.com>
2018-03-02 12:03:50 -0600

    Add functions for customizing scroll offset

--- site/static/js/smooth-scroll.js
+++ site/static/js/smooth-scroll.js
@@ -1,3 +1,29 @@
+// Get smooth scroll offset depending on window width
+function getWindowOffset() {
+  var BREAK_MOBILE = 600;
+  var BREAK_TABLET = 800;
+  var BREAK_DESKTOP = 1200;
+
+  var OFFSET_MOBILE = 60;
+  var OFFSET_TABLET = 80;
+  var OFFSET_DESKTOP = 80;
+
+  if (window.innerWidth < BREAK_MOBILE) {
+    return OFFSET_MOBILE;
+  } else if (window.innerWidthwidth < BREAK_TABLET) {
+    return OFFSET_TABLET;
+  } else {
+    return OFFSET_DESKTOP;
+  }
+}
+
+// Add an additional page offset where needed by checking the URL
+function getPageOffset(url) {
+  if (url.indexOf('help-faq') !== -1) {
+    return 40;
+  }
+  return 0;
+}
 
 $(function() {
     // This will select everything with the class smoothScroll
@@ -8,14 +34,10 @@ $(function() {
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
           $('html,body').animate({
-            scrollTop: target.offset().top -80 + 'px'
+            scrollTop: target.offset().top - (getWindowOffset() + getPageOffset(location.pathname)) + 'px'
           }, 1000); // The number here represents the speed of the scroll in milliseconds
           return false;
         }
       }
     });
-  });  
-  // Change the speed to whatever you want
-  // Personally i think 1000 is too much
-  // Try 800 or below, it seems not too much but it will make a difference
-  
+  });
