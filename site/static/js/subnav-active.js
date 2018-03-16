1c82991e3c5375c60a50b78e39db7250bd77d39f
James Minton <james-minton@github.com>
2018-03-15 21:42:34 -0500

    About Us nav tabs breakpoints

--- site/static/js/subnav-active.js
+++ site/static/js/subnav-active.js
@@ -1,4 +1,9 @@
-$(".subnav a").click(function () { 
-    $(".subnav a").css("color", "#050403").removeClass("highlight");
+$(".nav a").click(function () { 
+    $(".nav a").css("color", "#050403").removeClass("highlight");
     $(this).css("color", "#E24000").addClass("highlight");
   });
+
+  $(".subnav a").click(function () { 
+    $(".subnav a").css("color", "#050403").removeClass("highlight");
+    $(this).css("color", "#E24000").addClass("highlight");
+  });  
