5a7a5a12b4c3ed9392587be4e3d194a230404caa
James Minton <james-minton@github.com>
2018-03-18 19:06:59 -0500

    Add book to WEM

--- site/static/js/subnav-active.js
+++ site/static/js/subnav-active.js
@@ -8,3 +8,9 @@ $(".nav a").click(function () {
     $(".subnav a").css("color", "#050403").removeClass("highlight");
     $(this).css("color", "#E24000").addClass("highlight");
   });  
+
+  $(".alt-bg .nav a").click(function () { 
+    $(".nav a").css("color", "#fff").removeClass("highlight");
+    $(".nav li").removeClass("active");
+    $(this).css("color", "#E24000").addClass("highlight");
+  });
