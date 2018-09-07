67c539431f89a1ec173ae8faba2cc88ee791ef70
Lane Olson <laneolson@gmail.com>
2018-09-07 11:23:28 -0700

    update gulpfile, cleanup, update packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,82 +1,23 @@
 import gulp from "gulp";
+import sass from "gulp-sass";
+import cssnano from "cssnano";
 import {spawn} from "child_process";
 import hugoBin from "hugo-bin";
-import gutil from "gulp-util";
 import postcss from "gulp-postcss";
-import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
-import watch from "gulp-watch";
-import webpack from "webpack";
-import webpackConfig from "./webpack.conf";
-import sass from 'gulp-sass';
-import cssnano from 'cssnano';
 
 const browserSync = BrowserSync.create();
 
 // Hugo arguments
 const hugoArgsDefault = ["-d", "../dist", "-s", "site", "-v"];
-const hugoArgsPreview = ["--buildDrafts", "--buildFuture"];
-
-// Development tasks
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
-
-// Build/production tasks
-gulp.task("build", ["sass", "css", "js"], (cb) => buildSite(cb, [], "production"));
-gulp.task("build-preview", ["sass", "css", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
-
-gulp.task("sass", function() {
-  return gulp.src("./src/sass/main-sass.scss")
-    .pipe(sass()) // Using gulp-sass
-    .pipe(postcss([cssnext({browserslist: [ ">= 1% in US" ]}), cssnano()]))
-    .pipe(gulp.dest("./dist/css"));
-});
-
-// Compile CSS with PostCSS
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext({ browserslist: [ ">= 1% in US" ]})]))
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(browserSync.stream())
-));
-
-// Compile Javascript
-gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
-
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
-});
-
-// Development server with browsersync
-gulp.task("server", ["hugo", "css", "sass", "js"], () => {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
-  watch("./src/js/**/*.js", () => { gulp.start(["js"]) });
-  watch("./src/sass/**/*.scss", () => { gulp.start(["sass"]) });
-  watch("./src/css/**/*.css", () => { gulp.start(["css"]) });
-  watch("./site/**/*", () => { gulp.start(["hugo"]) });
-});
 
 /**
- * Run hugo and build the site
+ * Build the site using Hugo
  */
-function buildSite(cb, options, environment = "development") {
+const buildSite = (cb, options, environment = "development") => {
   const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
-
   process.env.NODE_ENV = environment;
-
   return spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
     if (code === 0) {
       browserSync.reload();
@@ -86,4 +27,60 @@ function buildSite(cb, options, environment = "development") {
       cb("Hugo build failed");
     }
   });
-}
+};
+
+/**
+ * Compile SASS styles for website
+ */
+const compileStyle = () => {
+  return gulp.src("./src/sass/main-sass.scss")
+    .pipe(sass()) // Using gulp-sass
+    .pipe(postcss([cssnext({browserslist: [ ">= 1% in US" ]}), cssnano()]))
+    .pipe(gulp.dest("./dist/css"));
+};
+
+/**
+ * Watch site files and recompile on changes
+ */
+const watchMarkup = () => {
+  return gulp.watch("./site/**/*", buildSite);
+};
+
+/**
+ * Watch styles and recompile on changes
+ */
+const watchStyle = () => {
+  return gulp.watch("./src/sass/**/*.scss", compileStyle);
+};
+
+const compile = gulp.parallel(buildSite, compileStyle);
+compile.description = "compile all sources";
+
+// Not exposed to CLI
+const startServer = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+};
+
+const server = gulp.series(compile, startServer);
+server.description = "serve compiled source on local server at port 3000";
+
+const watcher = gulp.parallel(watchMarkup, watchStyle);
+watcher.description = "watch for changes to all source";
+
+const defaultTasks = gulp.parallel(server, watcher);
+
+export {
+  compile,
+  buildSite,
+  compileStyle,
+  server,
+  watcher,
+  watchMarkup,
+  watchStyle
+};
+
+export default defaultTasks;
