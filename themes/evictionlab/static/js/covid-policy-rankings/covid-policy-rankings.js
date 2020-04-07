7549e4a3b3cefca76027acbf84d3411a430913c1
A Groshek <amy@amygroshek.com>
2020-04-07 10:34:42 -0500

    Adds sorting and filtering, makes display width of table smaller for mobile

--- themes/evictionlab/static/js/covid-policy-rankings/covid-policy-rankings.js
+++ themes/evictionlab/static/js/covid-policy-rankings/covid-policy-rankings.js
@@ -4,21 +4,135 @@ $(document).ready(function () {
   var rankings = {
     POLICY_SHEET_URL: 'https://spreadsheets.google.com/feeds/list/1_7NhsPRHMGhysge5SQYjEp8wHkQe0VoRIMYq_nB4a8U/1/public/values?alt=json',
     statesData: null,
-    statesArr: null,
-    statesArrFiltered: null,
-    sortStateData: function(sortBy) {
+    statesArr: [],
+    statesArrFiltered: [],
+    sortConfig: [], // 0 == sort by, 1 = asc, desc, or false
+    filterConfig: [],
+    sortStateData: function(callback) {
       console.log('sortStateData()');
+      // console.log('sortConfig, ', this.sortConfig);
+      var sortArr = rankings.sortConfig;
+      rankings.statesArrFiltered = rankings.statesArr;
+      if (sortArr.length > 0) {
+        console.log('there is a sort criterion');
+        if (sortArr[0] === 'stname') {
+          console.log('sorting by alpha');
+          if (sortArr[1] === 'asc') {
+            // console.log('sorting alpha ascending');
+            rankings.statesArrFiltered.sort(function (a, b) {
+              if (a[sortArr[0]] < b[sortArr[0]]) return -1;
+              else if (a[sortArr[0]] > b[sortArr[0]]) return 1;
+              return 0;
+            });
+          } else {
+            // console.log('sorting alpha descending');
+            rankings.statesArrFiltered.sort(function (a, b) {
+              if (a[sortArr[0]] > b[sortArr[0]]) return -1;
+              else if (a[sortArr[0]] < b[sortArr[0]]) return 1;
+              return 0;
+            });
+          }
+        } else {
+          // console.log('sorting by numeric');
+          if (sortArr[1] === 'asc') {
+            // console.log('sorting numeric ascending');
+            rankings.statesArrFiltered.sort(function(a, b) {
+              return parseFloat(a[sortArr[0]]) - parseFloat(b[sortArr[0]]);
+            });
+          } else {
+            // console.log('sorting numeric descending');
+            rankings.statesArrFiltered.sort(function(a, b) {
+              return parseFloat(b[sortArr[0]]) - parseFloat(a[sortArr[0]]);
+            });
+          }
+        }
+      } else {
+        // console.log('nothing to sort');
+        rankings.statesArrFiltered.sort(function (a, b) {
+          if (a.stname < b.stname) return -1;
+          else if (a.stname > b.stname) return 1;
+          return 0;
+        });
+      }
+      // console.log('done sorting. rankings.statesArrFiltered = ', rankings.statesArrFiltered);
+      callback();
     },
-    filterStateData: function(filters) {
+    filterStateData: function(callback) {
       console.log('filterStateData()');
+      console.log('filterConfig, ', this.filterConfig);
+      this.filterConfig.forEach(function(val, i) {
+        console.log('filtering for, ', val);
+        rankings.statesArrFiltered = rankings.statesArrFiltered.filter(function(el) {
+          console.log('el, ', el[val])
+          return el[val] === true;
+        })
+      });
+      callback();
+    },
+    sortAndFilter: function() {
+      // Daisy-chain sorting and filtering,
+      // and call populateStates when complete.
+      console.log('sortAndFilter()');
+      this.emptyStates(function() {
+        rankings.sortStateData(function() {
+          rankings.filterStateData(rankings.populateStates)
+        })
+      });
     },
     setStateListeners: function() {
       // Set click event listener for states.
     },
     setUIListeners: function() {
+      console.log('setUIListeners()');
       // Sets listeners for:
       // Table sort headings
+      $table_headings = $('#states_table > thead > tr > td');
+      $table_headings.on('click', function(e) {
+        console.log('sort clicked, ', e.currentTarget);
+        // Change sort status for clicked element
+        // Capture target.
+        var $target = $(e.currentTarget);
+        // Capture target's current sort state.
+        var oldState = String($target.attr('data-filter'));
+        // Reset all headings so we only sort by one column at a time.
+        $table_headings.attr('data-filter', 'false');
+        // console.log('sortState, ', sortState);
+        var newState = null;
+        if (oldState === 'false') {
+          // console.log('sortState === false');
+          newState = 'asc';
+          // $target.attr('data-filter', 'asc');
+        } else if (oldState === 'asc') {
+          newState = 'desc';
+          // $target.attr('data-filter', 'desc');
+        } else {
+          newState = 'false';
+          // $target.attr('data-filter', 'false');
+        }
+        $target.attr('data-filter', newState);
+        rankings.sortConfig = [];
+        if (newState !== 'false') {
+          rankings.sortConfig= [$target.attr('id'), newState];
+        }
+        rankings.sortAndFilter();
+        // console.log('rankings.sortConfig, ', rankings.sortConfig);
+      })
       // Filter checkboxes
+      $filters = $('#filters_panel .filters .filters-list input');
+      // console.log($filters);
+      $filters.on('change', function(e) {
+        // console.log('filter clicked, ', e.currentTarget);
+        // Gather sorting data.
+        rankings.filterConfig = [];
+        $filters.each(function(i) {
+          if ($(this).prop('checked')) {
+            rankings.filterConfig.push([$(this).attr('id')]);
+          }
+          // rankings.filterConfig[$(this).attr('id')] = $(this).prop('checked');
+        })
+        rankings.sortAndFilter();
+        // console.log('rankings.filterConfig, ', rankings.filterConfig);
+      })
     },
     loadHandlebarsTemplate: function(url, callback) {
       // Load handlebars template via xhr.
@@ -37,14 +151,18 @@ $(document).ready(function () {
     populateStateDetails: function(state) {
       console.log('populateStateDetails()');
     },
+    emptyStates: function(callback) {
+      $('#states_table tbody tr').hide('slow').remove();
+      callback();
+    },
     populateStates: function() {
-      console.log('populateStates(), ', this.statesArr)
+      console.log('populateStates(), ', rankings.statesArrFiltered)
       // Fetch and render handlebars template.
       // Always draws from filtered array.
-      var context = { states: this.statesArrFiltered };
+      var context = { states: rankings.statesArrFiltered };
       var url = '/js/covid-policy-rankings/states-template.html';
-      this.loadHandlebarsTemplate(url, function(template) {
-          $('#states_panel tbody').html(template(context));
+      rankings.loadHandlebarsTemplate(url, function(template) {
+          $('#states_panel tbody').html(template(context)).find('tr').show('slow');
       });
     },
     processData: function() {
@@ -69,7 +187,8 @@ $(document).ready(function () {
               }
             }
             if (p === 'stars') {
-              row[p] = String(row[p]).replace('.', '-');
+              row['stars'] = Number(row[p]);
+              row['stars_hb'] = String(row[p]).replace('.', '-');
             }
             if (row[p] === null) {
                 row[p] = '';
@@ -99,6 +218,7 @@ $(document).ready(function () {
     init: function() {
       console.log('init');
       this.loadStateData();
+      this.setUIListeners();
     }
   }
   rankings.init();
