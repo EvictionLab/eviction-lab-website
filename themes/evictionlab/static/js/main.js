fe08d8da98f29d0112d522904a09d589c8994613
Lane Olson <laneolson@gmail.com>
2020-05-13 09:24:36 -0600

    revert formatting on js

--- themes/evictionlab/static/js/main.js
+++ themes/evictionlab/static/js/main.js
@@ -1,23 +1,23 @@
 // Add toggling of menu
 function setupMenu() {
-  var menu = $(".app-menu");
-  var menuClose = $(".app-menu .menu-close");
-  var menuButton = $(".el-button-menu");
+  var menu = $('.app-menu');
+  var menuClose = $('.app-menu .menu-close');
+  var menuButton = $('.el-button-menu');
 
   menuButton.click(function () {
-    menu.addClass("expanded");
-    menu.find("a").each(function () {
-      $(this).attr("tabindex", null);
+    menu.addClass('expanded');
+    menu.find('a').each(function () {
+      $(this).attr('tabindex', null);
     });
-    menuClose.attr("tabindex", null);
+    menuClose.attr('tabindex', null);
   });
 
   menuClose.click(function () {
-    menu.removeClass("expanded");
-    menu.find("a").each(function () {
-      $(this).attr("tabindex", -1);
+    menu.removeClass('expanded');
+    menu.find('a').each(function () {
+      $(this).attr('tabindex', -1);
     });
-    menuClose.attr("tabindex", -1);
+    menuClose.attr('tabindex', -1);
   });
 }
 
@@ -25,44 +25,37 @@ function setupMenu() {
 function setupScrollEnd() {
   var debounceTime = 50;
   var isScrolling;
-  var headerWrapper = $("header");
+  var headerWrapper = $('header');
 
   function toggleCondense() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      headerWrapper.addClass("condensed");
-      $("body").addClass("header-condensed");
+      headerWrapper.addClass('condensed');
+      $('body').addClass('header-condensed');
     } else {
-      headerWrapper.removeClass("condensed");
-      $("body").removeClass("header-condensed");
+      headerWrapper.removeClass('condensed');
+      $('body').removeClass('header-condensed');
     }
     isScrolling = undefined;
   }
 
-  window.addEventListener(
-    "scroll",
-    function (event) {
-      if (isScrolling === undefined) {
-        toggleCondense();
-      } else {
-        window.clearTimeout(isScrolling);
-      }
-      isScrolling = setTimeout(toggleCondense, debounceTime);
-    },
-    false
-  );
+  window.addEventListener('scroll', function (event) {
+    if (isScrolling === undefined) {
+      toggleCondense();
+    } else {
+      window.clearTimeout(isScrolling);
+    }
+    isScrolling = setTimeout(toggleCondense, debounceTime);
+  }, false);
 }
 
 // Update contact form action with selected email path
 function setupContactForm() {
-  var form = $("#contactform");
-  var formActionBase = "https://formspree.io/";
-  form.attr(
-    "action",
-    formActionBase + $("input:radio[name=routing]:checked").data("email")
-  );
-  $(".contact-radio").click(function (el) {
+  var form = $('#contactform');
+  var formActionBase = 'https://formspree.io/';
+  form.attr('action', formActionBase + $("input:radio[name=routing]:checked").data('email'));
+  $('.contact-radio').click(function (el) {
     var selected = $(this);
-    form.attr("action", formActionBase + selected.data("email"));
+    form.attr('action', formActionBase + selected.data('email'));
   });
 }
 
@@ -70,7 +63,7 @@ function setupContactForm() {
 function setupModals() {
   $(".modal").on("show.bs.modal", function () {
     setTimeout(function () {
-      $("body").addClass("modal-open");
+      $("body").addClass("modal-open")
     }, 500);
   });
 }
@@ -84,7 +77,7 @@ var activeStates = [];
  */
 function emptyCovidTable() {
   // console.log('emptyCovidTable()');
-  return $("#covid-blog table tbody tr").hide().remove();
+  return $('#covid-blog table tbody tr').hide().remove();
 }
 
 /**
@@ -95,41 +88,25 @@ function populateCovidRows(items) {
   // console.log('populateCovidRows()');
   items.forEach((item) => {
     if (String(item.linktosourcenewspressreleaseetc).length > 30) {
-      item.linktruncated =
-        item.linktosourcenewspressreleaseetc.trim().substring(0, 30) + "...";
+      item.linktruncated = (item.linktosourcenewspressreleaseetc).trim().substring(0, 30)+ "...";
     }
-    var rowMarkup =
-      '<tr class="">' +
-      "<td>" +
-      (item.levelofgovernmentlocalstatenational !== "Local"
-        ? item.placename
-        : item.placenameformap + ", " + item.state) +
-      "</td>" +
-      "<td>" +
-      item.levelofgovernmentlocalstatenational +
-      "</td>" +
-      "<td>" +
-      item.typeofaction +
-      "</td>" +
-      "<td>" +
-      item.datesineffect +
-      "</td>" +
-      "<td>" +
-      (!!item.linktosourcenewspressreleaseetc
-        ? '<a href="' +
-          item.linktosourcenewspressreleaseetc +
-          '" target="_blank">' +
-          item.linktruncated +
-          "</a>"
-        : "") +
-      "</td>" +
-      "</tr>";
-    if ($("#covid-blog table tbody tr").length <= 0) {
-      $("#covid-blog table tbody").html(rowMarkup);
+    var rowMarkup = '<tr class="">' +
+      '<td>' +
+        (item.levelofgovernmentlocalstatenational !== 'Local' ?
+        item.placename :
+        item.placenameformap + ', ' + item.state) +
+      '</td>' +
+      '<td>' + item.levelofgovernmentlocalstatenational + '</td>' +
+      '<td>' + item.typeofaction + '</td>' +
+      '<td>' + item.datesineffect + '</td>' +
+      '<td>' + (!!item.linktosourcenewspressreleaseetc ? '<a href="' + item.linktosourcenewspressreleaseetc + '" target="_blank">' + item.linktruncated + '</a>' : '') + '</td>' +
+    '</tr>';
+    if ($('#covid-blog table tbody tr').length <= 0) {
+      $('#covid-blog table tbody').html(rowMarkup);
     } else {
-      $("#covid-blog table tbody tr").last().after(rowMarkup);
+      $('#covid-blog table tbody tr').last().after(rowMarkup);
     }
-  });
+  })
 }
 
 /**
@@ -141,24 +118,24 @@ function populateCovidRows(items) {
 function filterCovidTable(arr, filter) {
   // console.log('filterCovidTable()');
   var filteredArr = null;
-  if (filter === "all") {
+  if (filter === 'all') {
     // Federal, return all federal action types.
     filteredArr = arr;
-  } else if (filter === "allfederal") {
+  } else if (filter === 'allfederal') {
     // Federal, return all federal action types.
-    filteredArr = arr.filter(function (el) {
-      return el.levelofgovernmentlocalstatenational === "Federal";
-    });
-  } else if (filter === "allstate") {
+    filteredArr = arr.filter(function(el) {
+      return el.levelofgovernmentlocalstatenational === 'Federal';
+    })
+  } else if (filter === 'allstate') {
     // Return all state-level actions.
-    filteredArr = arr.filter(function (el) {
-      return el.levelofgovernmentlocalstatenational === "State";
-    });
+    filteredArr = arr.filter(function(el) {
+      return el.levelofgovernmentlocalstatenational === 'State';
+    })
   } else {
     // Filter for a particular state.
-    filteredArr = arr.filter(function (el) {
+    filteredArr = arr.filter(function(el) {
       return el.state === filter;
-    });
+    })
   }
   return filteredArr;
 }
@@ -173,30 +150,22 @@ function initCovidTable() {
     var rows = JSON.parse(req.responseText).feed.entry;
     // console.log('rows', rows);
     var properties = Object.keys(rows[0])
-      .filter(function (p) {
-        return p.startsWith("gsx$");
-      })
-      .map(function (p) {
-        return p.substr(4);
-      });
+        .filter(function (p) { return p.startsWith("gsx$"); })
+        .map(function (p) { return p.substr(4); });
     // console.log('properties', properties)
     var items = rows.map(function (r) {
       var row = {};
       properties.forEach(function (p) {
-        row[p.replace(".", "")] =
-          r["gsx$" + p].$t === "" ? null : r["gsx$" + p].$t;
-        if (row[p] === null) {
-          row[p] = "";
-        }
+          row[p.replace('.', '')] = r["gsx$" + p].$t === "" ? null : r["gsx$" + p].$t;
+          if (row[p] === null) {
+              row[p] = '';
+          }
       });
-      console.log("row", row);
-      if (
-        (row.levelofgovernmentlocalstatenational === "State") |
-        (row.levelofgovernmentlocalstatenational === "Local") |
-        (row.levelofgovernmentlocalstatenational === "Local/Federal District")
-      ) {
+      console.log('row', row)
+      if (row.levelofgovernmentlocalstatenational === 'State' | row.levelofgovernmentlocalstatenational === 'Local' |
+      row.levelofgovernmentlocalstatenational === 'Local/Federal District') {
         if (activeStates.indexOf(row.state) <= -1) {
-          activeStates.push(row.state);
+          activeStates.push(row.state)
         }
       }
       return row;
@@ -205,33 +174,27 @@ function initCovidTable() {
     covidItemsAll = items;
     populateCovidRows(covidItemsAll);
     // Remove disabled class from select items that should not be disabled.
-    activeStates.forEach(function (el, i) {
+    activeStates.forEach(function(el, i) {
       $('#filter_covid_table ul li > a[data-value="' + el + '"]')
-        .parent("li")
-        .removeClass("disabled");
-    });
+        .parent('li')
+        .removeClass('disabled');
+    })
     // Listen for select event.
-    $("#filter_covid_table ul li:not(.disabled) > a").on(
-      "select, click",
-      function (e) {
-        // console.log('Clicked or selected filter item.');
-        // console.log($(e.currentTarget).attr('data-value'));
-        var filterBy = $(e.currentTarget).attr("data-value");
-        emptyCovidTable();
-        populateCovidRows(filterCovidTable(covidItemsAll, filterBy));
-        $("span#selected_filter").text($(e.currentTarget).text());
-      }
-    );
+    $('#filter_covid_table ul li:not(.disabled) > a').on('select, click', function(e) {
+      // console.log('Clicked or selected filter item.');
+      // console.log($(e.currentTarget).attr('data-value'));
+      var filterBy = $(e.currentTarget).attr('data-value');
+      emptyCovidTable();
+      populateCovidRows(filterCovidTable(covidItemsAll, filterBy));
+      $('span#selected_filter').text($(e.currentTarget).text())
+    })
   }
 
-  // Sheet must be published as public
-  var SHEET_URL =
-    "https://spreadsheets.google.com/feeds/list/1XX9bBi_4ERpeqw_WyqHEIkfpShbif1i6HenBfymYv_U/1/public/values?alt=json";
+  // Sheet must be published as public 
+  var SHEET_URL = 'https://spreadsheets.google.com/feeds/list/1XX9bBi_4ERpeqw_WyqHEIkfpShbif1i6HenBfymYv_U/1/public/values?alt=json'
   // Fetch intervention data
   var mediaReq = new XMLHttpRequest();
-  mediaReq.addEventListener("load", function () {
-    reqHandler("media", mediaReq);
-  });
+  mediaReq.addEventListener("load",  function() { reqHandler('media', mediaReq) });
   mediaReq.open("GET", SHEET_URL);
   mediaReq.send();
 }
@@ -239,11 +202,11 @@ function initCovidTable() {
 $(document).ready(function () {
   setupMenu();
   setupScrollEnd();
-  if ($("#contactform").length) {
+  if ($('#contactform').length) {
     setupContactForm();
   }
   setupModals();
-  if ($("#covid-blog").length >= 1) {
+  if ($('#covid-blog').length >= 1) {
     // console.log('covid table is in the page.')
     initCovidTable();
   }
@@ -252,15 +215,17 @@ $(document).ready(function () {
 /**
  * Fill height
  * ----
- *
+ * 
  */
 $(document).ready(function () {
-  var el = $(".fill-height");
 
-  var setSplashHeight = function () {
-    var height = window.innerHeight - el.offset().top;
-    if (height > 1) el.css("height", height + "px");
-  };
+  var el = $('.fill-height')
+
+  var setSplashHeight = function() {
+    var height = window.innerHeight - el.offset().top
+    if (height > 1)
+      el.css('height', height + 'px')
+  }
   if (el.length) {
     window.addEventListener("resize", setSplashHeight);
     setSplashHeight();
@@ -270,7 +235,7 @@ $(document).ready(function () {
 /**
  * Smooth Scroll
  * ---
- *
+ * 
  */
 // Get smooth scroll offset depending on window width
 function getWindowOffset() {
@@ -293,51 +258,42 @@ function getWindowOffset() {
 
 // Add an additional page offset where needed by checking the URL
 function getPageOffset(url) {
-  if (url.indexOf("help-faq") !== -1) {
+  if (url.indexOf('help-faq') !== -1) {
     return 80;
   }
-  if (url.indexOf("methods") !== -1) {
+  if (url.indexOf('methods') !== -1) {
     return 80;
   }
-  if (url.indexOf("about") !== -1) {
+  if (url.indexOf('about') !== -1) {
     return 80;
   }
-  if (url.indexOf("why-eviction-matters") !== -1) {
+  if (url.indexOf('why-eviction-matters') !== -1) {
     return 80;
   }
-  if (url.indexOf("covid-policy-scorecard") !== -1) {
+  if (url.indexOf('covid-policy-scorecard') !== -1) {
     return 80;
   }
   return 0;
 }
 
+
 function smoothScroll(path, cb) {
   if (
-    location.pathname.replace(/^\//, "") == path.pathname.replace(/^\//, "") &&
+    location.pathname.replace(/^\//, '') == path.pathname.replace(/^\//, '') &&
     location.hostname == path.hostname &&
     path.hash.length
   ) {
     // check for accordion link
     var target = $('[href="' + path.hash + '"].collapsed');
     // if no accordion link look for an element with the ID
-    if (target.length === 0) {
-      target = $(path.hash);
-    }
+    if (target.length === 0) { target = $(path.hash); }
     // if no ID element look for an element with the name
-    if (target.length === 0) {
-      target = $('[name="' + path.hash.slice(1) + '"]');
-    }
+    if (target.length === 0) { target = $('[name="' + path.hash.slice(1) + '"]'); }
     // scroll to the target
     if (target.length) {
-      $("html,body").animate(
-        {
-          scrollTop:
-            target.offset().top -
-            (getWindowOffset() + getPageOffset(location.pathname)) +
-            "px",
-        },
-        1000
-      ); // The number here represents the speed of the scroll in milliseconds
+      $('html,body').animate({
+        scrollTop: target.offset().top - (getWindowOffset() + getPageOffset(location.pathname)) + 'px'
+      }, 1000); // The number here represents the speed of the scroll in milliseconds
       if (cb) cb(target);
       return false;
     }
@@ -348,20 +304,17 @@ $(function () {
   // Run smoothscroll on page load
   smoothScroll(window.location, function (target) {
     // If link is an accordion, toggle it
-    if (target.hasClass("collapsed")) {
-      target.collapse("toggle");
-      $(target.attr("href")).collapse("toggle");
+    if (target.hasClass('collapsed')) {
+      target.collapse('toggle');
+      $(target.attr('href')).collapse('toggle');
     }
   });
 
-  $(".smoothScroll").click(function () {
+  $('.smoothScroll').click(function () {
     smoothScroll(this);
     // Close dropdown if dropdown link
-    if ($(this).hasClass("dropdown-item")) {
-      $(this)
-        .closest("div.dropdown")
-        .find(".dropdown-toggle")
-        .dropdown("toggle");
+    if ($(this).hasClass('dropdown-item')) {
+      $(this).closest('div.dropdown').find('.dropdown-toggle').dropdown('toggle');
     }
   });
 });
@@ -369,31 +322,28 @@ $(function () {
 /**
  * Update URL on accordion expand
  */
-$(function () {
-  $(".accordion-section .collapsed").click(function () {
+$(function() {
+  $('.accordion-section .collapsed').click(function() {
     if (history.pushState) {
-      history.replaceState(null, null, $(this).attr("href"));
-    } else {
-      location.hash = $(this).attr("href");
+      history.replaceState(null, null, $(this).attr('href'));
+    }
+    else {
+      location.hash = $(this).attr('href');
     }
   });
 });
 
-/**
- * Social Share Popup
+/** 
+ * Social Share Popup 
  * ---
  * Add event listeners to all social share popups that open links in a popup window
  */
 $(document).ready(function () {
-  $("a.social-share-popup").each(function () {
+  $('a.social-share-popup').each(function () {
     var el = $(this);
-    el.on("click", function (e) {
+    el.on('click', function (e) {
       e.preventDefault();
-      window.open(
-        el.attr("href"),
-        "Social Share",
-        "height=285,width=550,resizable=1"
-      );
+      window.open(el.attr('href'), 'Social Share', 'height=285,width=550,resizable=1');
     });
   });
 });
@@ -424,20 +374,13 @@ $(document).ready(function () {
 /**
  * Google Sheets-backed media table
  */
-function createMediaTable(
-  oneLastUpdated,
-  oneRowCount,
-  lastUpdated,
-  rows,
-  totalLMSReferrals
-) {
+function createMediaTable(oneLastUpdated, oneRowCount, lastUpdated, rows, totalLMSReferrals) {
   var updatedOne = Date.parse(oneLastUpdated.slice(0, 10));
   var updatedTwo = Date.parse(lastUpdated.slice(0, 10));
   var dateNum = updatedOne >= updatedTwo ? updatedOne : updatedTwo;
   var updatedDate = new Date(dateNum);
   var updatedStr = updatedDate.toLocaleDateString("en-US");
-  var lmsReferralStr =
-    totalLMSReferrals > 0 ? ", " + totalLMSReferrals + " LMS referrals" : "";
+  var lmsReferralStr = totalLMSReferrals > 0 ? ', ' + totalLMSReferrals + ' LMS referrals' : ''
 
   $("#lastUpdated").html(updatedStr);
   $("#numLocalStories").html(oneRowCount.toLocaleString());
@@ -458,13 +401,7 @@ function createMediaTable(
     var tr = $("<tr></tr>");
     for (var pi = 0; pi < props.length; ++pi) {
       if (props[pi] === "title") {
-        var td = $(
-          "<td><a href='" +
-            rows[i].link +
-            "' target='_blank'>" +
-            rows[i].title +
-            "</a></td>"
-        );
+        var td = $("<td><a href='" + rows[i].link + "' target='_blank'>" + rows[i].title + "</a></td>")
       } else {
         var td = $("<td>" + rows[i][props[pi]] + "</td>");
       }
@@ -478,12 +415,11 @@ function createMediaTable(
  * Loading Google Sheets national media table, set up map embed
  */
 $(document).ready(function () {
-  var SHEET_BASE =
-    "https://spreadsheets.google.com/feeds/list/1fIHfkpG134ax5neHQGfipUrWbzPd-EAlVVlYqm4i9iE/";
-  var SHEET_END = "/public/values?alt=json";
-  var SHEET_1_URL = SHEET_BASE + "1" + SHEET_END;
-  var SHEET_2_URL = SHEET_BASE + "2" + SHEET_END;
-  var SHEET_LMS_URL = SHEET_BASE + "3" + SHEET_END;
+  var SHEET_BASE = 'https://spreadsheets.google.com/feeds/list/1fIHfkpG134ax5neHQGfipUrWbzPd-EAlVVlYqm4i9iE/';
+  var SHEET_END = '/public/values?alt=json';
+  var SHEET_1_URL = SHEET_BASE + '1' + SHEET_END;
+  var SHEET_2_URL = SHEET_BASE + '2' + SHEET_END;
+  var SHEET_LMS_URL = SHEET_BASE + '3' + SHEET_END;
 
   if ($("#national-media-table").length) {
     $.getJSON(SHEET_1_URL, function (data) {
@@ -494,35 +430,27 @@ $(document).ready(function () {
         var rows = data.feed.entry;
         var lastUpdated = data.feed.updated.$t;
         var properties = Object.keys(rows[0])
-          .filter(function (p) {
-            return p.startsWith("gsx$");
-          })
-          .map(function (p) {
-            return p.substr(4);
-          });
+          .filter(function (p) { return p.startsWith("gsx$"); })
+          .map(function (p) { return p.substr(4); });
 
         var cleanedRows = rows.map(function (r) {
           var row = {};
           properties.forEach(function (p) {
             row[p] = r["gsx$" + p].$t === "" ? null : r["gsx$" + p].$t;
             if (row[p] === null) {
-              row[p] = "";
+              row[p] = '';
             }
           });
           return row;
         });
 
         $.getJSON(SHEET_LMS_URL, function (data) {
-          var totalLMSReferrals = data.feed.entry.length;
+          var totalLMSReferrals = data.feed.entry.length
 
-          createMediaTable(
-            sheetOneUpdated,
-            sheetOneRowCount,
-            lastUpdated,
-            cleanedRows,
-            totalLMSReferrals
-          );
+          createMediaTable(sheetOneUpdated, sheetOneRowCount, lastUpdated, cleanedRows, totalLMSReferrals);
         });
+
+
       });
     });
   }
