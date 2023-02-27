505d300b09c1f0fb62452898ec66e1c225770baf
Josh Maas-Howard <j.maas.howard@gmail.com>
2023-02-27 17:50:44 -0600

    feat: update map generation scripts - add script for calculating bbox

---
+++ scripts/addMapBbox.js
@@ -0,0 +1,37 @@
+/*
+ * ADD BBOX (so map zooms to correct area)
+ * example usage (from this directory):
+ *  node addMapBbox.js input.json output_shapes.json
+ *
+ * Assumes that all map features have a properties.bounds array.
+ * To generate these, open the map in mapshaper, open the Console and run:
+ *  each bounds=this.bounds
+ * Then export as GEOJson
+ */
+
+const fs = require("fs");
+const input = process.argv[2];
+const output = process.argv[3];
+const fullStateJson = require(input);
+
+function addMapBbox(fullStateJson) {
+  const bbox = fullStateJson.features.reduce(
+    (accum, f) => {
+      const fBounds = f.properties.bounds;
+      // delete property, which is no longer needed
+      delete f.properties.bounds;
+      return [
+        Math.min(accum[0], fBounds[0]),
+        Math.min(accum[1], fBounds[1]),
+        Math.max(accum[2], fBounds[2]),
+        Math.max(accum[3], fBounds[3]),
+      ];
+    },
+    [Infinity, Infinity, -Infinity, -Infinity],
+  );
+
+  fullStateJson.bbox = bbox;
+  fs.writeFileSync(output, JSON.stringify(fullStateJson));
+}
+
+addMapBbox(fullStateJson);
