704723648a0071e23bf0ccadc18fb7ad10ae2764
Josh Maas-Howard <j.maas.howard@gmail.com>
2025-08-27 12:39:40 -0700

    feat: change trends line default to vs baseline

--- themes/evictionlab/assets/eviction-tracking/app.js
+++ themes/evictionlab/assets/eviction-tracking/app.js
@@ -3255,7 +3255,7 @@ Elab.Trends = (function (Elab) {
 
   var xCol = "month";
   var yCols = ["month_filings", "percentage_diff"];
-  var yCol = yCols[0];
+  var yCol = yCols[1];
   var avgCol = "avg_filings";
   var showLast12 = true;
   var allData;
